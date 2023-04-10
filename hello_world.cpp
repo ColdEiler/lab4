17359c75909f58f27b53e0f15cf49aac2f278edc
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-04-10 21:49:19 +0300

    hello_world.cpp was modified

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-using namespace std;
 
-int main(int argc, char){
-    cout << "Hello, world!" << endl;
+
+int main(){
+    char name[20];
+	std::cout << "What's your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Hello world form " << name << std::endl;
+	return 0;    
 }
