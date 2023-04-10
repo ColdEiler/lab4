dc54976a53e6e08f36b63d6108df607968aea98d
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-04-10 22:30:10 +0300

    hello_world.cpp add comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+<<<<<<< HEAD
 
+=======
+using namespace std;
+// comment // 
+>>>>>>> 5a517dd (hello_world.cpp add comment)
 int main(){
     char name[20];
 	std::cout << "What's your name?" << std::endl;
